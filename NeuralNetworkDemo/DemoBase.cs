cd5038f93399489782b9444de19dfd6af3dcf404
Baudi Kurbanov <screwyuguys@gmail.com>
2025-09-08 02:48:14 +0300

    feature: added console demo for the project

---
+++ NeuralNetworkDemo/DemoBase.cs
@@ -0,0 +1,17 @@
+﻿using KKNeuralNetwork;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkDemo;
+internal abstract class DemoBase
+{
+	internal abstract void Run();
+	protected abstract string Path { get; }
+	public abstract string Name { get; }
+	internal abstract void Train(NeuralNetwork nn);
+	internal abstract void Test(NeuralNetwork nn);
+}
